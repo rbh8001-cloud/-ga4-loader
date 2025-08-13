916664d4d9d7bc54da8fd1d6d42ed29781d92b7d
rbh8001-cloud <bobh338@gmail.com>
2025-08-13 11:13:32 -0400

    Create  ga4-loader.js

---
+++ ga4-loader.js
@@ -0,0 +1,13 @@
+(function() {  
+  var gtagScript = document.createElement('script');
+  gtagScript.async = true;
+  gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-ZC93LJZLLF';
+  document.head.appendChild(gtagScript);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  window.gtag = gtag;
+
+  gtag('js', new Date());
+  gtag('config', 'G-ZC93LJZLLF');
+})();
